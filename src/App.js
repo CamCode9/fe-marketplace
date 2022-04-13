0b5c07259b630d47a13c2627e1ec892a43ea8a8f
Cam Lindsay <cameronlindsay@Cams-MacBook-Pro.local>
2022-04-13 13:02:44 +0100

    working on category endpoint - filter not working correctly in ItemByCat

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Items from "./components/AllItems";
+import ItemByCat from "./components/ItemByCat";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">Test</header>
       <Routes>
         <Route path="/api/items" element={<Items />} />
+        <Route path="/api/items/:category_name" element={<ItemByCat />} />
       </Routes>
     </div>
   );
--- src/components/ItemByCat.jsx
+++ src/components/ItemByCat.jsx
@@ -2,10 +2,36 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 export default function ItemByCat() {
-  const { item_cat } = useParams();
+  const { category_name } = useParams();
+
   const [itemCat, setItemCat] = useState([]);
 
   useEffect(() => {
-    fetch(`https://space-facts.herokuapp.com/api/planets/`);
-  });
+    fetch(`https://jim-cam-marketplace.herokuapp.com/api/items/`)
+    .then(res => res.json())
+    .then(data => setItemCat(data.items));
+  }, [category_name]);
+
+  console.log(itemCat, 'PRE')
+
+  const categoryItems = itemCat.filter(item => {
+    if(item.category_name === category_name) {
+      return item;
+    }
+  })
+
+  console.log(categoryItems, 'ITEMS BY CAT')
+  // return (
+  //   <div>
+  //     <h1>Items in {category_name} category:</h1>
+  //     <ul>
+  //       {itemCat.filter((item) => {
+  //         return <li key={item.item_id}>
+
+  //         </li>
+  //       })}
+  //     </ul>
+      
+  //   </div>
+  // )
 }
